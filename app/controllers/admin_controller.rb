4144c2876648f5b301050b334a708436822ab7ab
nreavill <nreavill@gmail.com>
2010-02-25 16:30:58 +0000

    layout changes; fix indenting

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,16 +1,16 @@
 class AdminController < ApplicationController
   
   uses_tiny_mce :only => [:new, :create, :edit, :update],
-                :options => {:theme => 'advanced',
-    :browsers => %w{msie gecko},
+    :options => {:theme => 'advanced',
     :theme_advanced_toolbar_location => "top",
     :theme_advanced_toolbar_align => "left",
     :theme_advanced_resizing => true,
     :theme_advanced_resize_horizontal => false,
-    :theme_advanced_buttons1 => %w{formatselect bold italic underline justifyleft justifycenter justifyright bullist numlist separator link unlink code indent outdent},
+    :theme_advanced_buttons1 => %w{formatselect styleselect bold italic underline justifyleft justifycenter justifyright bullist numlist separator link unlink code indent outdent},
     :theme_advanced_buttons2 => [],
     :theme_advanced_buttons3 => [],
-    :extended_valid_elements => "a[name|href|target|title|onclick]"
+    :extended_valid_elements => "a[name|href|target|title|onclick]",
+    :theme_advanced_styles => "First Line Indent=first_line_indent"
   }
   
   before_filter :login_required
