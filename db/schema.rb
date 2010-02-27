e0400b6bbfe29b633e50def6f40532098ad06cf0
nreavill <nreavill@gmail.com>
2010-02-27 09:45:21 +0000

    add epigraph to works

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091206191848) do
+ActiveRecord::Schema.define(:version => 20100227093411) do
 
   create_table "editions", :force => true do |t|
     t.string   "title"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(:version => 20091206191848) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "epigraph"
   end
 
 end
